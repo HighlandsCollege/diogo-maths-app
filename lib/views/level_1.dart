accf87ca962a17822c97c5cfa81a5505de9e27d1
Diogo <Diogosaguiar@hotmail.com>
2021-03-24 11:51:21 +0000

    created first page

---
+++ lib/views/level_1.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Level1 extends StatefulWidget {
+  Level1({Key key}) : super(key:key);
+
+  @override
+  _Level1 createState() => _Level1();  
+}
+
+class _Level1 extends State<Level1> {
+  @override 
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Text('mathsQuestion')
+      )
+    );
+  }
+}
